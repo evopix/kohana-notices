5803045866a10380939c9bab09e95c1a342e7190
Brandon Summers <brandon@evolutionpixels.com>
2011-01-02 00:32:25 -0800

    Removed all image related code.

--- classes/view/notice.php
+++ classes/view/notice.php
@@ -13,17 +13,9 @@ class View_Notice extends Kostache {
 			'type_formatted' => ucwords(__($this->_notice->type)),
 			'persistant'     => $this->_notice->is_persistent ? ' notice-persistent' : '',
 			'message'        => $this->_notice->message,
-			
-			'image'          => Notices::image(
-				$this->_notice->type, 
-				array('width' => 32, 'height' => 32, 'alt' => ucwords(__($this->_notice->type)))
-			),
-			
 			'remove_link'    => HTML::anchor(
 				Route::get('notice-remove')->uri(array('hash' => $this->_notice->hash)),
-				HTML::image(
-					'media/images/notices/notice-close.png',
-					array('width' => 16, 'height' => 16, 'alt' => __('Close'))),
+				__('Close'),
 				array('title' => __('Close'))
 			),
 		);
